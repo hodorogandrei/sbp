1026a77a680e6a794e6da35145a3b6892732964b
Andrei Hodorog <hodorog.andrei@gmail.com>
2016-10-04 09:31:26 +0100

    First version of multiple filtering working

--- src/js/maps.js
+++ src/js/maps.js
@@ -40,61 +40,98 @@ var googleMaps = function(){
             // Draw the sensors
             sensors.draw(map);
 
-            function setMapOnAll(map, markers) {
+            var setMapOnAll = function(map, markers) {
                 for (var i = 0; i < markers.length; i++) {
                     markers[i].setMap(map);
                 }
+            };
+
+            var encode = function(string) {
+                return encodeURIComponent(string).replace(/'/g,"%27").replace(/"/g,"%22");
             }
 
             // Draw all objects for Tywyn
-            var tywynObj = allObjects.fetch('http://131.251.176.109:8082/ontology/tywyn/select?query=%20PREFIX%20wis%3A%3Chttp%3A%2F%2Fwww.WISDOM.org%2FWISDOMontology%23%3E%0APREFIX%20rdf%3A%3Chttp%3A%2F%2Fwww.w3.org%2F1999%2F02%2F22-rdf-syntax-ns%23%3E%0ASELECT%20%20%3FXcoord%20%3FYcoord%20%3Fclass%0AWHERE%20%7B%20%0A%3FURI%20wis%3AhasXcoord%20%3FXcoord%20.%0A%3FURI%20wis%3AhasYcoord%20%3FYcoord%20.%0A%3FURI%20a%20%3Fclass%20.%0A%7D');
-            var tywynMarkers = [];
-            for (var i = 0; i < tywynObj.length; i++) {
-                var marker = new google.maps.Marker({
-                  position: {lat: tywynObj[i].lat, lng: tywynObj[i].lon},
-                  lat: tywynObj[i].lat,
-                  lng: tywynObj[i].lon
-                });
-                tywynMarkers.push(marker);
-            }
-            setMapOnAll(map, tywynMarkers);
+            // var tywynObj = allObjects.fetch('http://131.251.176.109:8082/ontology/tywyn/select?query=%20PREFIX%20wis%3A%3Chttp%3A%2F%2Fwww.WISDOM.org%2FWISDOMontology%23%3E%0APREFIX%20rdf%3A%3Chttp%3A%2F%2Fwww.w3.org%2F1999%2F02%2F22-rdf-syntax-ns%23%3E%0ASELECT%20%20%3FXcoord%20%3FYcoord%20%3Fclass%0AWHERE%20%7B%20%0A%3FURI%20wis%3AhasXcoord%20%3FXcoord%20.%0A%3FURI%20wis%3AhasYcoord%20%3FYcoord%20.%0A%3FURI%20a%20%3Fclass%20.%0A%7D');
+            // var tywynMarkers = [];
+            // for (var i = 0; i < tywynObj.length; i++) {
+            //     var marker = new google.maps.Marker({
+            //       position: {lat: tywynObj[i].lat, lng: tywynObj[i].lon},
+            //       lat: tywynObj[i].lat,
+            //       lng: tywynObj[i].lon
+            //     });
+            //     tywynMarkers.push(marker);
+            // }
+            // setMapOnAll(map, tywynMarkers);
 
-            // Draw water objects for Tywyn
-            var tywynWaterObj = allObjects.fetch('http://131.251.176.109:8082/ontology/tywyn/select?query=PREFIX%20wis%3A%3Chttp%3A%2F%2Fwww.WISDOM.org%2FWISDOMontology%23%3E%0APREFIX%20rdf%3A%3Chttp%3A%2F%2Fwww.w3.org%2F1999%2F02%2F22-rdf-syntax-ns%23%3E%0ASELECT%20%20%3FXcoord%20%3FYcoord%20%3Fclass%0AWHERE%20%7B%20%0A%3FURI%20wis%3AhasXcoord%20%3FXcoord%20.%0A%3FURI%20wis%3AhasYcoord%20%3FYcoord%20.%0A%3FURI%20a%20wis%3AWaterMeter%20.%0A%7D');
-            var tywynWaterMarkers = [];
-            for (var i = 0; i < tywynWaterObj.length; i++) {
-                var marker = new google.maps.Marker({
-                  position: {lat: tywynWaterObj[i].lat, lng: tywynWaterObj[i].lon},
-                  lat: tywynWaterObj[i].lat,
-                  lng: tywynWaterObj[i].lon
-                });
-                tywynWaterMarkers.push(marker);
-            }
+            // // Draw water objects for Tywyn
+            // var tywynWaterObj = allObjects.fetch('http://131.251.176.109:8082/ontology/tywyn/select?query=PREFIX%20wis%3A%3Chttp%3A%2F%2Fwww.WISDOM.org%2FWISDOMontology%23%3E%0APREFIX%20rdf%3A%3Chttp%3A%2F%2Fwww.w3.org%2F1999%2F02%2F22-rdf-syntax-ns%23%3E%0ASELECT%20%20%3FXcoord%20%3FYcoord%20%3Fclass%0AWHERE%20%7B%20%0A%3FURI%20wis%3AhasXcoord%20%3FXcoord%20.%0A%3FURI%20wis%3AhasYcoord%20%3FYcoord%20.%0A%3FURI%20a%20wis%3AWaterMeter%20.%0A%7D');
+            // var tywynWaterMarkers = [];
+            // for (var i = 0; i < tywynWaterObj.length; i++) {
+            //     var marker = new google.maps.Marker({
+            //       position: {lat: tywynWaterObj[i].lat, lng: tywynWaterObj[i].lon},
+            //       lat: tywynWaterObj[i].lat,
+            //       lng: tywynWaterObj[i].lon
+            //     });
+            //     tywynWaterMarkers.push(marker);
+            // }
 
-            // Draw all objects for Gower
-            var gowerObj = allObjects.fetch('http://131.251.176.109:8082/ontology/gower/select?query=%20PREFIX%20wis%3A%3Chttp%3A%2F%2Fwww.WISDOM.org%2FWISDOMontology%23%3E%0APREFIX%20rdf%3A%3Chttp%3A%2F%2Fwww.w3.org%2F1999%2F02%2F22-rdf-syntax-ns%23%3E%0ASELECT%20%20%3FXcoord%20%3FYcoord%20%3Fclass%0AWHERE%20%7B%20%0A%3FURI%20wis%3AhasXcoord%20%3FXcoord%20.%0A%3FURI%20wis%3AhasYcoord%20%3FYcoord%20.%0A%3FURI%20a%20%3Fclass%20.%0A%7D');
-            var gowerMarkers = [];
-            for (var i = 0; i < gowerObj.length; i++) {
-                var marker = new google.maps.Marker({
-                  position: {lat: gowerObj[i].lat, lng: gowerObj[i].lon},
-                  lat: gowerObj[i].lat,
-                  lng: gowerObj[i].lon
-                });
-                gowerMarkers.push(marker);
-            }
-            setMapOnAll(map, gowerMarkers);
+            // // Draw all objects for Gower
+            // var gowerObj = allObjects.fetch('http://131.251.176.109:8082/ontology/gower/select?query=%20PREFIX%20wis%3A%3Chttp%3A%2F%2Fwww.WISDOM.org%2FWISDOMontology%23%3E%0APREFIX%20rdf%3A%3Chttp%3A%2F%2Fwww.w3.org%2F1999%2F02%2F22-rdf-syntax-ns%23%3E%0ASELECT%20%20%3FXcoord%20%3FYcoord%20%3Fclass%0AWHERE%20%7B%20%0A%3FURI%20wis%3AhasXcoord%20%3FXcoord%20.%0A%3FURI%20wis%3AhasYcoord%20%3FYcoord%20.%0A%3FURI%20a%20%3Fclass%20.%0A%7D');
+            // var gowerMarkers = [];
+            // for (var i = 0; i < gowerObj.length; i++) {
+            //     var marker = new google.maps.Marker({
+            //       position: {lat: gowerObj[i].lat, lng: gowerObj[i].lon},
+            //       lat: gowerObj[i].lat,
+            //       lng: gowerObj[i].lon
+            //     });
+            //     gowerMarkers.push(marker);
+            // }
+            // setMapOnAll(map, gowerMarkers);
 
-            $('html').on('click', '#show-all', function() {
-                setMapOnAll(null, tywynWaterMarkers);
-                setMapOnAll(map, tywynMarkers);
-                setMapOnAll(map, gowerMarkers);
+            var selectedItems = [];
+            $('html').on('change', '.data-filter', function() {
+                selectedItems = [];
+                $('.data-filter option:selected').each(function(){ selectedItems.push($(this).data('filter')); });
             });
 
-            $('html').on('click', '#water-meter', function() {
-                setMapOnAll(null, tywynMarkers);
-                setMapOnAll(null, gowerMarkers);
-                setMapOnAll(map, tywynWaterMarkers);
+            var queryTemplate = 'PREFIX wis:<http://www.WISDOM.org/WISDOMontology#>' + '\n' +
+                                'PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>' + '\n' +
+                                'SELECT  ?Xcoord ?Ycoord ?class' + '\n' +
+                                'WHERE {' + '\n' +
+                                '?URI wis:hasXcoord ?Xcoord .' + '\n' +
+                                '?URI wis:hasYcoord ?Ycoord .',
+                queryToSend,
+                rawObj,
+                markers = [],
+                baseUrl = 'http://131.251.176.109:8082/ontology/';
+
+            $('html').on('click', '#filter', function() {
+                console.log(selectedItems);
+                queryToSend = queryTemplate + '\n' + '{?URI a wis:' + selectedItems[0] + ' . }';
+                for (var i = 1; i < selectedItems.length; i++) {
+                    queryToSend += ' UNION {?URI a wis:' + selectedItems[i] + ' . }';
+                }
+
+                queryToSend += '\n' + '}';
+                queryToSend = encode(queryToSend);
+
+                if(markers.length) {
+                    setMapOnAll(null, markers);
+                }
+
+                rawObj = allObjects.fetch(baseUrl + 'gower/select?query=' + queryToSend);
+                markers = [];
+                for (var i = 0; i < rawObj.length; i++) {
+                    var marker = new google.maps.Marker({
+                      position: {lat: rawObj[i].lat, lng: rawObj[i].lon},
+                      lat: rawObj[i].lat,
+                      lng: rawObj[i].lon
+                    });
+                    markers.push(marker);
+                }
+                setMapOnAll(map, markers);
             });
+
         }
         loadScriptAsync(mapsScriptUrl);
 
